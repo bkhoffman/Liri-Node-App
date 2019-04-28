5c33be5388e7e00ad4eef861a8c48ce21c42d83f
boffman <bkhoffman@gmail.com>
2019-04-27 22:42:05 -0500

    added Mr Nobody as default for movie, added some comments

--- liri.js
+++ liri.js
@@ -11,6 +11,11 @@ let userInput = process.argv.slice(3).join(" ");
 
 //Pass "choice" arg into movieInfo function query OMDB api, log Data
 const movieInfo = function(choice){
+  if(!choice) {
+    choice = "Mr. Nobody";
+    console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/\nIt's on Netflix!");
+    console.log("**".repeat(40))
+	}
   let movieName = choice;
   let queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
   axios.get(queryUrl).then(
@@ -45,7 +50,7 @@ const spotifyInfo = function(choice){
     const song = data.tracks.items;
     // console.log(song);
     for(let i = 0; i < song.length; i++){
-      console.log("Song Result:");
+      console.log("Song Info:");
       console.log("Artist: " + song[i].artists[0].name);  
       console.log("Song Name: " + song[i].name); 
       console.log("Preview Link: " + song[i].preview_url); 
@@ -78,7 +83,7 @@ const bandInfo = function(choice){
     }
   );
 };
-
+//When "do-what-it-says" is the command, this func reads random.txt file and what's in it. Then passes that info as user choices
 const randomInfo = function(){
   var fs = require("fs");
   fs.readFile("random.txt", "utf8", function(error, data){
@@ -96,7 +101,7 @@ const randomInfo = function(){
 //Pass user input into Switch function to invoke appropriate function
 //Pass user choice as args into those fuctions to send Requests
 const userChoices = function(type, choice){
-  console.log("User Choices: " + type, choice);
+  // console.log("User Choices: " + type, choice);
   switch(type){
     case 'movie-this': movieInfo(choice);
     break;
